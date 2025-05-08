21ef83f9f35b775ba7534226c40d36769e376716
rhit-bakerlm <bakerlm@rose-hulman.edu>
2025-05-08 10:44:17 -0400

    Initial commit

--- SSSP.cpp
+++ SSSP.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 class Vertex {
     public:
     int value;
-    std::unordered_map<Vertex, int> edges;
+    std::unordered_map<Vertex*, int> edges;
 
     //For pathfinding
     long d;
@@ -26,11 +28,11 @@ class Graph {
     }
 };
 
-std::vector<Vertex> topoSort(Graph graph, Vertex start) {
+/*std::vector<Vertex> topoSort(Graph graph, Vertex start) {
     std::unordered_map<Vertex, bool> visited;
 
 
-}
+}*/
 
 //Pseduocode for Single-Source Shortest Path (SSSP) algorithms
 
@@ -43,7 +45,7 @@ std::vector<Vertex> topoSort(Graph graph, Vertex start) {
 // 		v.pi = null
 // 	s.d = 0
 
-void initializeSingleSource(Graph graph, Vertex start) {
+void initializeSingleSource(Graph graph, Vertex* start) {
     for (int i = 0; i < graph.vertices.size(); i++) {
         graph.vertices[i].d = 10000000;
         graph.vertices[i].pi = nullptr;
@@ -60,10 +62,10 @@ void initializeSingleSource(Graph graph, Vertex start) {
 // 		v.d = u.d + w(u, v)
 // 		v.pi = u
 
-void relax(Vertex u, Vertex v) {
-    if (v.d > u.d + u.edges.at(v)) {
-        v.d = u.d + u.edges.at(v);
-        v.pi = &u;
+void relax(Vertex* u, Vertex* v) {
+    if (v->d > u->d + u->edges.at(v)) {
+        v->d = u->d + u->edges.at(v);
+        v->pi = u;
     }
 }
 
@@ -85,20 +87,20 @@ void relax(Vertex u, Vertex v) {
 // 			return FALSE
 // 	return TRUE
 
-bool bellmanFord(Graph g, Vertex s) {
+bool bellmanFord(Graph g, Vertex* s) {
     initializeSingleSource(g, s);
 
     for (int i = 0; i < g.vertices.size() - 1; i++) {
         for (int j = 0; j < g.vertices.size(); j++) {
             for (const auto& entry : g.vertices[j].edges) {
-                relax(g.vertices[j], entry.first);
+                relax(&g.vertices[j], entry.first);
             }
         }
     }
 
     for (int j = 0; j < g.vertices.size(); j++) {
         for (const auto& entry : g.vertices[j].edges) {
-            if (entry.first.d > g.vertices[j].d + entry.second) return false;
+            if (entry.first->d > g.vertices[j].d + entry.second) return false;
         }
     }
     
@@ -116,17 +118,17 @@ bool bellmanFord(Graph g, Vertex s) {
 // 		for each vertex v adjacent to u (looping edges (u, v))
 // 			RELAX(u, v)	
 
-void dagShortestPaths(Graph g, Vertex s) {
-    std::vector<Vertex> sortedGraph;
+// void dagShortestPaths(Graph g, Vertex s) {
+//     std::vector<Vertex> sortedGraph;
 
-    sortedGraph = topoSort(g, s);
+//     //sortedGraph = topoSort(g, s);
 
-    for (const auto& u : sortedGraph) {
-        for (const auto& v : u.edges) {
-            relax(u, v.first);
-        }
-    }
-}
+//     for (const auto& u : sortedGraph) {
+//         for (const auto& v : u.edges) {
+//             relax(u, v.first);
+//         }
+//     }
+// }
 
 /*Runs Dijkstra's algorithms to compute all SSSP distances
 # for directed graph G and source vertex s.
@@ -144,6 +146,13 @@ void dagShortestPaths(Graph g, Vertex s) {
 // 		for each vertex v adjacent to u  (for all edges (u, v))
 // 			RELAX(u, v)
 
+void dijkstra(Graph g, Vertex* s) {
+    initializeSingleSource(g, s);
+
+    std::unordered_set<Vertex> S;
+    
+}
+
 
 /*Prints the shortest path from source vertex s to vertex v in
 # directed graph G.
@@ -159,15 +168,15 @@ void dagShortestPaths(Graph g, Vertex s) {
 // 		PRINT-PATH(G, s, v.pi)
 // 		print v
 
-void printPath(Graph g, Vertex s, Vertex v) {
-    if (&s == &v)  {
-        std::cout << s.value;
+void printPath(Graph g, Vertex* s, Vertex* v) {
+    if (s == v)  {
+        std::cout << s->value << std::endl;
     }
-    else if (v.pi == nullptr) {
-        std::cout << "no path";
+    else if (v->pi == nullptr) {
+        std::cout << "Impossible" << std::endl;
     }
     else {
-        printPath(g, s, *v.pi);
+        printPath(g, s, v->pi);
     }
 }
 
@@ -189,16 +198,16 @@ int main() {
             int u, v, w;
             std::cin >> u >> v >> w;
 
-            graph.vertices[u].edges.insert({graph.vertices[v], w});
+            graph.vertices[u].edges.insert({&graph.vertices[v], w});
         }
 
-        bellmanFord(graph, graph.vertices[s]);
+        //bellmanFord(graph, &graph.vertices[s]);
 
         for (int i = 0; i < queries; i++) {
             int q;
             std::cin >> q;
 
-            printPath(graph, graph.vertices[s], graph.vertices[q]);
+            printPath(graph, &graph.vertices[s], &graph.vertices[q]);
         }
     }
 }
